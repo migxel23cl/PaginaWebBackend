ecc79f898b26e92ff0389b0cc3537fc495292813
CETECOM <CETECOM@AVAWSV01LC0102.duoc.acad>
2024-06-19 16:04:36 -0400

    avance
    123

--- pagweb/WYLB/models.py
+++ pagweb/WYLB/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+ class Usuario(models.Model):
+    id = models.AutoField(max_length=5)
+    nombre = models.CharField(max_length=20)
+    apellido_paterno = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, unique=True, blank=True, null=True)
+    password = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50, blank=True, null=True)
+    activo = models.BooleanField()
