d4d6fdd2ce251110a025c4c1c8fcd5fb85deb411
migxel23cl <mi.pereirap@duocuc.cl>
2024-07-03 20:37:28 -0400

    backup
    123

--- pagweb/WYLB/urls.py
+++ pagweb/WYLB/urls.py
@@ -13,10 +13,10 @@ Function views
 
 urlpatterns=[
     path('',views.index, name="index"),
-    path('login',views.login, name="login"),
-    path('registro',views.registro, name="registro"),
-    path('gusto',views.gusto, name="gusto"),
-    path('productos',views.productos, name="productos"),
-    path('productoU',views.productoU, name="productoU"),
-    path('ventanaPago',views.ventanaPago, name="ventanaPago"),
+    path('login/',views.login, name="login"),
+    path('registro/',views.registro, name="registro"),
+    path('gusto/',views.gusto, name="gusto"),
+    path('productos/',views.productos, name="productos"),
+    path('productoU/',views.productoU, name="productoU"),
+    path('ventanaPago/',views.ventanaPago, name="ventanaPago"),
 ]
